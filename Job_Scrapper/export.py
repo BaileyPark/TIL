cf8545426bd7e4f02e6fccce8214a42cdf06651b
WorkaHolic <baileypark@kakao.com>
2022-03-01 08:29:05 +0900

    change which

--- Job_Scrapper/export.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def save_to_file(jobs, word):
-  file = open(f"{word}.csv", mode="w")
-  writer = csv.writer(file)
-  writer.writerow(["Tilte", "Company", "Location", "Link"])
-  for job in jobs:
-    writer.writerow(job)
-  return
